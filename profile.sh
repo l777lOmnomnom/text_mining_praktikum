4787cbe6391ace834fff3f47f12658664a716215
Robby Wagner <robby.wagner1991@web.de>
2019-11-16 15:12:29 +0100

    Initial commit with venv and profiler

---
+++ profile.sh
@@ -0,0 +1,2 @@
+# Usage: make executeable (chmod +x profile.sh) -> execute .profile.sh your/script.py
+python3 -m cProfile $1
---
+++ test.py
@@ -0,0 +1,9 @@
+import simhash
+
+def test(a, i):
+    a = i * (a + 1)
+
+
+if __name__ == "__main__":
+    for i in range(100):
+        test(0, i)
