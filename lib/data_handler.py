5deaaa395ee39ea17a44387bcbb7c55a92f5daf7
Robby Wagner <robby.wagner1991@web.de>
2019-11-26 17:26:29 +0100

    datahandler is now a Class

--- lib/data_handler.py
+++ lib/data_handler.py
@@ -2,133 +2,208 @@ import re
 import os
 import json
 import subprocess
-import warcio
-import argparse
+# import warcio
+# import argparse
 
-FILE = os.path.dirname(os.path.abspath(__file__))
 
-# BESXHREIBÄHDKFH
-def get_header_body_dict(file, elements=1000, offset=0):
-    """ ### Should be especially used for test purpose ###
-    Returns a dictonary containing elements amount of entries with {html_header_info: html_text}.
-    The offset and elements can be used to iterate through a large file step by step instead of loading it all.
-    :param file:
-    :param elements:
-    :param offset:
-    :return:
+class DataHandlerException(Exception):
     """
-    return __read_source(file, elements, offset)
-
-
-def update_database_jaccard(datasets, database=os.path.join(FILE, "data/db")):
-    """
-
-    :param data:
-    :return:
-    """
-    new_data = dict()
-    for dataset in datasets:
-        new_data.update(dataset.dump())
-
-    with open(database, "r") as db:
-        try:
-            old_data = json.load(db)
-        except Exception:
-            old_data = dict()
-
-    new_data.update(old_data)
-
-    with open(database, "w") as db:
-        json.dump(new_data, db)
-
-
-def __read_source(input_file, elements=500, offset=0):
-    """
-    Reads data from an extracted warc.gz and yields a dict with {str(header): str(body)}
-    There is a default max element size of 1000 and an offset if you want to get elements somewhere in the data set.
-
-    :param input_file: path of the extracted source file
-    :param elements: the amount of elements you want to retrieve
-    :param offset:
-    :return: result_dict
-    """
-    line = None  # Placeholder
-    return_dict = dict()
-
-    if input_file.split(".")[-1] is "gz":
-        print("Input file is an archive. Extracting it now ...")
-        input_file = __extract_archive(input_file)
-
-    with open(input_file, 'r') as source:
-
-        current_elements = 0
-        skipped_elements = 0
-        source_iterator = iter(source)
-
-        # Offsets the iterator
-        if offset != 0:
-            print(offset)
-            while skipped_elements <= int(offset):
-                line = next(source_iterator)
-                if re.search("<source><location>", line):
-                    skipped_elements += 1
-
-            header = line  # Otherwise the header wouldn't be available bellow as the next method continues
-
-        # Create the actual dict with the data extries from offset to elements + offset
-        while current_elements < elements:
-            line = next(source_iterator)
-
-            if re.search("<source><location>", line):
-                header = line
-                current_elements += 1
-
-            #  This is stupid but it works
-            elif line is "\n" or id(line) == 140591544343968:
-                continue
-
-            else:
-                # If you want to add your own return format add the return objects definition in the beginning of the
-                # function and fill it here with the payload here (or with header infos above) (return x, y, z).
-                body = return_dict.get(header, "")
-                return_dict.update({header: "{} {}".format(body, line)})
-
-    return return_dict
-
-
-# Somehow there is a problem with pathfinding here
-def __extract_archive(input_file, output_file="/tmp/output.source"):
-    """
-    Extracts a warc.gz file. Can also be called from command line via :
-
-    $ python3 datahandler.py -i input_file -o output_file
-
-    :param input_file: Path to the warc.gz archive
-    :param output_file: Path to the destination file (default: /tmp/output.source)
-    :return:
-    """
-    current_file_loc = os.path.dirname(os.path.abspath(__file__))
-
-    if not os.path.isfile(input_file):
-        raise FileNotFoundError("File {} not found!".format(input_file))
-
-    if os.path.isfile(output_file):
-        print("Found existing output file ({}). Renaming it to {}.old and continue ...".format(output_file, output_file))
-        os.rename(output_file, "{}.old".format(output_file))
-
-    tool_path = os.path.join(current_file_loc, '..', 'tools', 'jwarcex-standalone-2.0.0.jar')
-    cmd = "java -jar {} {} {} -c".format(tool_path, input_file, output_file)
-
-    print(cmd)
+    This a common convention to have a file specific exception. Usage:
 
     try:
-        subprocess.check_call(cmd)
-    except subprocess.CalledProcessError as err:
-        print("It looks like there is an issue with path-finding. Copy the link and execute it manually ...")
-        print("There was an errror executing the command. Stopping now!")
-        print(err)
-        exit(1)
-    else:
-        print("Successfully extracted {} to {}!".format(input_file, output_file))
+        your_code()
+    except SomeExceptionRaisedByYourCode as err:
+        raise DataHandlerException(err)
 
-    return output_file
+    It raises the original error but as DataHandlerException to make the failing part more visible
+    """
+    pass
+
+
+class __DataHandler:
+    def __init__(self, config):
+        self.__input = config.get("input")
+        self.__output = config.get("output")
+        self.__database = config.get("database")
+
+    @property
+    def input(self):
+        return self.__input
+
+    @input.setter
+    def input(self, __i):
+        self.__input = __i
+
+    @property
+    def output(self):
+        return self.__output
+
+    @output.setter
+    def output(self, __o):
+        self.__output = __o
+
+    @property
+    def database(self):
+        return self.__database
+
+    @database.setter
+    def database(self, __d):
+        self.__database = __d
+
+    def update_database(self, data):
+        """
+
+        :param data:
+        :return:
+        """
+        new_data = dict()
+
+        try:
+            for dataset in data:
+                new_data.update(dataset.dump())
+
+            with open(self.database, "r") as db:
+                try:
+                    old_data = json.load(db)
+                except json.JSONDecodeError:
+                    old_data = dict()
+
+            new_data.update(old_data)
+
+            with open(self.database, "w") as db:
+                json.dump(new_data, db)
+        except Exception as err:
+            print("There was a problem updating the database {} with {} \n{}".format(self.database, data, err))
+            return False
+
+        return True
+
+
+class DataHandlerSimHash(__DataHandler):
+    def __init__(self, config):
+        super(DataHandlerSimHash, self).__init__(config)
+
+
+class DataHandlerMinHash(__DataHandler):
+    def __init__(self, config):
+        super(DataHandlerMinHash, self).__init__(config)
+
+        if self.input.split(".")[-1] == "gz":
+            print("Input file is an archive. Extracting it to /tmp/output.source")
+            self.input = self.__extract_archive(self.input)
+
+    def get_data(self, elements, offset):
+        """
+
+        :param elements:
+        :param offset:
+        :return:
+        """
+        return_dict, is_finished_flag = self.__read_source(self.input, elements, offset)
+
+        return return_dict, is_finished_flag
+
+    def update_database(self, data):
+        """
+
+        :param data:
+        :return:
+        """
+        return super().update_database(data)
+
+    @staticmethod
+    def __read_source(input_file, elements, offset=0):
+        """
+        Reads data from an extracted or compressed warc archive (.source | .warc.gz) and returns a dictonary with
+        {source_address1: source_body1, ...}
+        If elements if given it will only read so many entries until the given amount is reached.
+        If offset if given the returned entries will start with an offset of entries shifted.
+        This can be used to iterate through the data without using to much storage.
+
+        :param input_file: path of the extracted source file
+        :param elements: the amount of elements you want to retrieve
+        :param offset: the offset from the beginning of the file
+        :return: result_dict
+        """
+        line = None  # Placeholder
+        is_finished_flag = False
+        return_dict = dict()
+
+        with open(input_file, 'r') as source:
+            current_elements = 0
+            skipped_elements = 0
+            source_iterator = iter(source)
+            try:
+                # Offsets the iterator
+                if offset != 0:
+                    print(offset)
+                    while skipped_elements <= int(offset):
+                        line = next(source_iterator)
+                        if re.search("<source><location>", line):
+                            skipped_elements += 1
+
+                    # Otherwise the header wouldn't be available bellow as the next method continues
+                    header = line.split("</location>")[0].replace("<source><location>", "")
+
+                # Create the actual dict with the data extries from offset to elements + offset
+                while current_elements < elements:
+                    line = next(source_iterator)
+
+                    if re.search("<source><location>", line):
+                        header = line.split("</location>")[0].replace("<source><location>", "")
+                        current_elements += 1
+                        print(current_elements)
+
+                    #  This is stupid but it works
+                    elif line is "\n" or id(line) == 140591544343968:
+                        continue
+
+                    else:
+                        body = return_dict.get(header, "")
+                        return_dict.update({header: "{} {}".format(body, line)})
+            except StopIteration:
+                is_finished_flag = True
+                return return_dict, is_finished_flag
+            except Exception as err:
+                raise DataHandlerException(err)
+
+        return return_dict, is_finished_flag
+
+    # Somehow there is a problem with pathfinding here
+    @staticmethod
+    def __extract_archive(input_file, output_file="/tmp/output.source"):
+        """
+        Extracts a warc.gz file. Can also be called from command line via :
+
+        $ python3 datahandler.py -i input_file -o output_file
+
+        :param input_file: Path to the warc.gz archive
+        :param output_file: Path to the destination file (default: /tmp/output.source)
+        :return:
+        """
+        current_file_loc = os.path.dirname(os.path.abspath(__file__))
+
+        if not os.path.isfile(input_file):
+            raise FileNotFoundError("File {} not found!".format(input_file))
+
+        if os.path.isfile(output_file):
+            print("Found existing output file ({}). Renaming it to {}.old and continue ...".format(output_file,
+                                                                                                   output_file))
+            os.rename(output_file, "{}.old".format(output_file))
+
+        tool_path = os.path.join(current_file_loc, '..', 'tools', 'jwarcex-standalone-2.0.0.jar')
+        cmd = "java -jar {} {} {} -c".format(tool_path, input_file, output_file)
+
+        print(cmd)
+
+        try:
+            subprocess.check_call(cmd)
+        except subprocess.CalledProcessError as err:
+            print("It looks like there is an issue with path-finding. Copy the link and execute it manually ...")
+            print("There was an errror executing the command. Stopping now!")
+            print(err)
+            exit(1)
+        else:
+            print("Successfully extracted {} to {}!".format(input_file, output_file))
+
+        return output_file
--- near_duplicate_detection/jaccard.py
+++ near_duplicate_detection/jaccard.py
@@ -6,60 +6,74 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),".."))
 from datasketch import MinHash
 from lib import data_handler
 
-# BESCHREIBUNG
+
 class JaccardSim:
     def __init__(self, config):
-        self.__input_file = config["input"]
-        self.__database = config["database"]
+        self.data_handler = data_handler.DataHandlerMinHash(config)
+
+        self.__elements = config.get("elements", 100)
+        self.__offset = config.get("offset", 0)
+        self.__mode = config.get("mode")
+        self.__tracker = 0
 
         # Currently limited of a small subset of data
-        self.__data = data_handler.get_header_body_dict(self.__input_file)
+        self.__data = dict()
+
+    @property
+    def mode(self):
+        return self.__mode
+
+    @property
+    def tracker(self):
+        self.__tracker += 1
+        return self.__tracker
+
+    @property
+    def elements(self):
+        return self.__elements
+
+    @property
+    def offset(self):
+        return self.__offset
+
+    @offset.setter
+    def offset(self, _o):
+        self.__offset = _o
 
     @property
     def data(self):
         return self.__data
 
-    @property
-    def database(self):
-        return self.__database
+    @data.setter
+    def data(self, _d):
+        self.__data = _d
 
-    @staticmethod
-    def init_dataset(dataset):
-        """
-
-        :param datset:
-        :return:
-        """
-        dataset_list = list()
-
-        i = 0
-        for header1, body1 in dataset.items():
-            i += 1
-            j = 0
-            for header2, body2 in dataset.items():
-                j += 1
-                if j > i:
-                    dataset_list.append(Dataset(header1, header2 ,body1, body2))
-
-        return dataset_list
-
-    def calculate_jaccard_sim(self):
+    def main(self):
         """
 
         :return:
         """
-        sets_dict = dict()
+        print("Current element size: {}, current offset: {}".format(self.elements, self.offset))
+        self.data, is_finished_flag = self.data_handler.get_data(self.elements, self.offset)
+        if self.mode == "both_jaccard_sim":
+            self.calculate_jaccard_sim()
+            self.estimate_jaccard_sim()
+        else:
+            getattr(self, self.mode)()
 
-        for source, words in self.data.items():
-            sets_dict.update({str(source): set(words.split(" "))})
+        self.offset += self.elements
 
-        datasets = self.init_dataset(sets_dict)
+        while not is_finished_flag:
+            print("Current element size: {}, current offset: {}".format(self.elements, self.offset))
 
-        for dataset in datasets:
-            jaccard_sim = self.__calculate_jaccard_sim(dataset.body_tuple)
-            dataset.calc_jaccard_sim = jaccard_sim
+            if self.mode == "both_jaccard_sim":
+                self.estimate_jaccard_sim()
+                self.calculate_jaccard_sim()
+            else:
+                getattr(self, self.mode)(self.elements, self.offset)
 
-        data_handler.update_database_jaccard(datasets, self.database)
+            self.offset += self.elements
+            self.data, is_finished_flag = self.data_handler.get_data(self.elements, self.offset)
 
         return
 
@@ -74,14 +88,71 @@ class JaccardSim:
             for word in words:
                 m.update(word.encode('utf8'))
             sets_dict.update({str(source): m})
+        print(sets_dict)
 
-        datasets = self.init_dataset(sets_dict)
+        datasets = self.__init_dataset(sets_dict)
 
         for dataset in datasets:
             jaccard_sim = self.__estimate_jaccard_sim(dataset.body_tuple)
+
+            print("Calculations done: {}".format(self.tracker))
+
             dataset.est_jaccard_sim = jaccard_sim
 
-        data_handler.update_database_jaccard(datasets, self.database)
+        self.data_handler.update_database(datasets)
+
+        return
+
+    @staticmethod
+    def __estimate_jaccard_sim(body_tuple):
+        """
+
+        :param body_tuple:
+        :return:
+        """
+        return body_tuple[0].jaccard(body_tuple[1])
+
+    @staticmethod
+    def __init_dataset(dataset):
+        """
+
+        :param dataset:
+        :return:
+        """
+        dataset_list = list()
+
+        i = 0
+        for header1, body1 in dataset.items():
+            i += 1
+            j = 0
+            for header2, body2 in dataset.items():
+                j += 1
+                if j > i:
+                    dataset_list.append(Dataset(header1, header2, body1, body2))
+
+        return dataset_list
+
+    def calculate_jaccard_sim(self):
+        """
+
+        :return:
+        """
+
+        sets_dict = dict()
+
+        for source, words in self.data.items():
+            sets_dict.update({str(source): set(words.split(" "))})
+
+        datasets = self.__init_dataset(sets_dict)
+
+        for dataset in datasets:
+            jaccard_sim = self.__calculate_jaccard_sim(dataset.body_tuple)
+
+            print("Calculations done: {}".format(self.tracker))
+
+            dataset.calc_jaccard_sim = jaccard_sim
+
+        self.data_handler.update_database(datasets)
 
         return
 
@@ -91,24 +162,7 @@ class JaccardSim:
 
         :return:
         """
-
-        actual_jaccard = float(len(body_tuple[0].intersection(body_tuple[1]))) / float(len(body_tuple[0].union(body_tuple[1])))
-        print("Actual Jaccard for data1 and data2 is", actual_jaccard)
-
-        return actual_jaccard
-
-    @staticmethod
-    def __estimate_jaccard_sim(body_tuple):
-        """
-
-        :param minhash_sets_list:
-        :return:
-        """
-        jaccard_sim = body_tuple[0].jaccard(body_tuple[1])
-        print("Estimated Jaccard for data1 and data2 is", jaccard_sim)
-
-        return jaccard_sim
-
+        return float(len(body_tuple[0].intersection(body_tuple[1]))) / float(len(body_tuple[0].union(body_tuple[1])))
 
 class Dataset:
     def __init__(self, header1, header2, body1, body2):
@@ -116,8 +170,8 @@ class Dataset:
         self.__body_tuple = (body1, body2)
         self.__est_jaccard_sim = None
         self.__calc_jaccard_sim = None
-        self.__est_jaccard_time = None
-        self.__calc_jaccard_time = None
+        self.__est_jaccard_time = None  # Not implemented
+        self.__calc_jaccard_time = None  # Not implemented
 
     @property
     def header_tuple(self):
@@ -152,14 +206,4 @@ class Dataset:
         self.__calc_jaccard_sim = sim
 
     def dump(self):
-        return {self.header_tuple: {"est_jaccard_sim": self.est_jaccard_sim,
-                                    "est_jaccard_time": self.est_jaccard_time,
-                                    "calc_jaccard_sim": self.calc_jaccard_sim,
-                                    "calc_jaccard_time": self.calc_jaccard_time}}
-
-
-if __name__ == "__main__":
-    pass
-    #m = JaccardSim("examples/output.source")
-    #m = JaccardSim(os.path.join(os.path.dirname(os.path.abspath(__file__)),"..","examples","output.source"))
-
+        return {self.header_tuple: {"est_jaccard_sim": self.est_jaccard_sim}}
